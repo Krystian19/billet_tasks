b2305fa2c9d7c0a1793f3d1d6abdc677832ffd48
Jan Guzman <janfrancisco19@gmail.com>
2024-01-28 16:21:40 -0400

    reorganize task assignment routes

--- src/app/routers/tasks.py
+++ src/app/routers/tasks.py
@@ -1,5 +1,5 @@
 from app.db.connection import get_db
-from app.crud import tasks 
+from app.crud import tasks, users, users2tasks 
 from app.routers.schemas import TaskSchema, MutationResponse, TaskFieldsSchema
 
 from fastapi import APIRouter, Depends, status
@@ -25,6 +25,33 @@ def update_task(taskId: int, task: TaskFieldsSchema,  db: Session = Depends(get_
 
     updated_task = tasks.update(db, taskId=taskId, task=task)
     return { "success": True, "detail": "updated task successfully", "payload": TaskSchema(**updated_task.__dict__) }
+
+
+@router.put("/v1/tasks/{taskId}/assign/user/{userId}", response_model=MutationResponse)
+def assign_task_2_user(taskId: int, userId: int, db: Session = Depends(get_db)):
+    found_user = users.get_one(db, userId)
+    if found_user is None:
+        return { "success": False, "detail": "specified user does not exist" }
+
+    found_task = tasks.get_one(db, taskId) 
+    if found_task is None:
+        return { "success": False, "detail": "specified task does not exist" }
+    
+    res = { "success": True, "detail": "successfully assigned task" }
+    found_assignment = users2tasks.get_one(db, userId=userId, taskId=taskId)
+
+    # NOTE: make sure the same task never gets assigned twice, probably it's a better
+    # idea to achieve the same behavior through db constraints
+    if found_assignment is None:
+        return res
+
+    users2tasks.create(db, userId=userId, taskId=taskId)
+    return res
+
+@router.put("/v1/tasks/{taskId}/unassign/user/{userId}", response_model=MutationResponse)
+def unassign_task_from_user(taskId: int, userId: int, db: Session = Depends(get_db)):
+    users2tasks.destroy(db, userId=userId, taskId=taskId)
+    return { "success": True, "detail": "successfully unassigned task" }
 
 @router.delete("/v1/tasks/{taskId}", response_model=MutationResponse)
 def delete_task(taskId:int, db: Session = Depends(get_db)):
--- src/app/routers/users.py
+++ src/app/routers/users.py
@@ -1,5 +1,5 @@
 from app.db.connection import get_db
-from app.crud import users, users2tasks, tasks
+from app.crud import users
 from app.routers.schemas import UserSchema, TaskSchema, MutationResponse
 
 from fastapi import APIRouter, Depends
@@ -15,32 +15,6 @@ def get_all_users(db: Session = Depends(get_db)):
 @router.get("/v1/users/{userId}/tasks", response_model=List[TaskSchema])
 def get_user_tasks(userId: int, db: Session = Depends(get_db)):
     return users.get_user_tasks(db, userId)
-
-@router.post("/v1/users/{userId}/assign/task/{taskId}", response_model=MutationResponse)
-def assign_task_2_user(userId: int, taskId: int, db: Session = Depends(get_db)):
-    found_user = users.get_one(db, userId)
-    if found_user is None:
-        return { "success": False, "detail": "specified user does not exist" }
-
-    found_task = tasks.get_one(db, taskId) 
-    if found_task is None:
-        return { "success": False, "detail": "specified task does not exist" }
-    
-    res = { "success": True, "detail": "successfully assigned task" }
-    found_assignment = users2tasks.get_one(db, userId=userId, taskId=taskId)
-
-    # NOTE: make sure the same task never gets assigned twice, probably it's a better
-    # idea to achieve the same behavior through db constraints
-    if found_assignment is None:
-        return res
-
-    users2tasks.create(db, userId=userId, taskId=taskId)
-    return res
-
-@router.delete("/v1/users/{userId}/unassign/task/{taskId}", response_model=MutationResponse)
-def unassign_task_from_user(userId: int, taskId: int, db: Session = Depends(get_db)):
-    users2tasks.destroy(db, userId=userId, taskId=taskId)
-    return { "success": True, "detail": "successfully unassigned task" }
 
 @router.delete("/v1/users/{userId}", response_model=MutationResponse)
 def delete_user(userId: int, db: Session = Depends(get_db)):
